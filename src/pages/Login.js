4328a8dfb657ecf9eb5ff7d9d5ad8534536cb24e
nazariy17 <nazariy2410@gmail.com>
2018-03-27 15:31:50 +0300

    login page layout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,16 +3,25 @@ import React from 'react';
 
 
 class Login extends React.Component {
+
+    login (){
+
+    }
+
     render() {
         return (
-            <div className="ui center aligned middle aligned grid">
-                <div className="column">
-                    <h2 className="ui teal center aligned header">Вхід в систему</h2>
-                    <form action="" className="ui large form">
+            <div className="ui center aligned middle aligned grid"  >
+                <div className="six wide column ">
+                    <h2 className="ui center aligned header">Вхід в систему</h2>
+                    <form action="/" className="ui large form">
                         <div className="ui stacked segment">
                             <div className="field">
-                                <input type="text"/>
+                                <div className="ui fluid left icon input">
+                                    <input type="text" placeholder="Enter your login" />
+                                    <i className="user icon" />
+                                </div>
                             </div>
+                            <button type="submit" className="ui primary large fluid button" onSubmit={this.login}>Увійти</button>
                         </div>
                     </form>
                 </div>
