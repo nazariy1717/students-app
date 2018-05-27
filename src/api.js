9a4c331d2139177bbe646e2c0457cbcfeb0ccf28
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-05-27 19:52:03 +0300

    changes

--- src/api.js
+++ src/api.js
@@ -3,17 +3,14 @@ import axios from 'axios';
 export default {
     admin: {
         login: (credentials) => axios.post('/api/admin-auth',{credentials}).then(res => res.data.admin),
-
         /* Group */
         addGroup: (data) => axios.post('/api/addGroup',{data}).then(res => res.data),
         getGroup: () => axios.get('/api/getGroup').then(res => res.data),
         removeGroup: (group) => axios.post('/api/removeGroup',{group}).then(res => res.data),
-
         /* Student */
         addStudent: (student) => axios.post('/api/addStudent',{student}).then(res => res.data),
         getStudents: () => axios.get('/api/getStudents').then(res => res.data),
         removeStudent: (student) => axios.post('/api/removeStudent',{student}).then(res => res.data),
-
         /* Teacher */
         addTeacher: (teacher) => axios.post('/api/addTeacher',{teacher}).then(res => res.data),
         getTeachers: () => axios.get('/api/getTeachers').then(res => res.data),
@@ -23,17 +20,13 @@ export default {
         addLesson: (lesson) => axios.post('/api/addLesson',{lesson}).then(res => res.data),
         getLesson: () => axios.get('/api/getLesson').then(res => res.data),
         removeLesson: (lesson) => axios.post('/api/removeLesson',{lesson}).then(res => res.data),
-
     },
-
     student: {
         login: (credentials) => axios.post('/api/student-auth',{credentials}).then(res => res.data)
     },
-
     teacher: {
         login: (credentials) => axios.post('/api/teacher-auth',{credentials}).then(res => res.data)
     },
-
     user: {
         login: (credentials) => axios.post('/api/auth',{credentials}).then(res => res.data.user)
     }
